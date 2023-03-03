6a9cfda722769ae1f33993c69dc1661fae7aeca3
santi <santi@sanferar.com>
2023-03-03 17:22:34 +0100

    Diferentes tamaños del contador

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './hojas-estilo/App.css';
 import React from 'react';
-import Timer from './componentes/timer.jsx';
+import Timer1 from './componentes/timer1.jsx';
+import Timer2 from './componentes/timer2.jsx';
+import Timer3 from './componentes/timer3.jsx';
 
 function App() {
 
-//La siguiente constante para ajustar el tiempo del temporizador
-const tiempoInicial = 60;
+  //La siguiente constante para ajustar el tiempo del temporizador
+  const tiempoInicialTimer1 = 60;
+  const tiempoInicialTimer2 = 120;
+  const tiempoInicialTimer3 = 240;
 
-  return(
+  return (
     <div className='contenedor-padre'>
-       <Timer 
-       tiempoInicial = {tiempoInicial} />
+      <Timer1
+        tiempoInicial={tiempoInicialTimer1} />
+
+      <Timer2
+        tiempoInicial={tiempoInicialTimer2} />
+
+      <Timer3 tiempoInicial={tiempoInicialTimer3} />
+
+
+
     </div>
-   
+
   )
 }
 
